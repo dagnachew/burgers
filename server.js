9ce8e50a181514255f7ec471077ecee3c9fbf53b
Seifu <dagnachew@gmail.com>
2019-09-26 22:38:03 -0500

    edit to server.js

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.set("view engine", "handlebars");
 const routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
-
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
 });
